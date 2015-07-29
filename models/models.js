22e289818ca7576e67bbda15de5ae4018b497e4e
JART2015 <jartwhat@gmail.com>
2015-07-29 17:52:20 +0200

    Creacion de Load

--- models/models.js
+++ models/models.js
@@ -37,13 +37,11 @@ sequelize.sync().then(function(){
 	//  success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count){
 		if(count === 0){   //la tabla se inicializa solo si esta vacia
-		   Quiz.create({ pregunta: 'Capital de Italia',
-						 respuesta: 'Roma'
-						 });
-			Quiz.create({ pregunta: 'Capital de portugal',
-						 respuesta: 'Lisboa'
-						 });
-			/*.then(function(){console.log('Base de datos inicializada')})*/
+		   Quiz.bulkCreate([
+						{ pregunta: 'Capital de Italia', respuesta: 'Roma' },
+						{ pregunta: 'Capital de portugal', respuesta: 'Lisboa'}
+						]
+					).then(function(){console.log('Base de datos inicializada')})
 			};
 	});
 });
