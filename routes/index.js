9cff917cce6ed58c1060e382c04320ea50008f09
JART2015 <jartwhat@gmail.com>
2015-08-05 19:21:00 +0200

    Gestion de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/', function(req, res) {
 });
 
 //Autoload de comandos con :quizId
-router.param('quizId',quizController.load);  //autoload :quizId
+router.param('quizId',    quizController.load);  //autoload :quizId
+router.param('commentId', commentController.load);  //autoload :commentId
 
 //Definicion de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -32,7 +33,9 @@ router.get('/author',function(req,res){
 	res.render('author',{errors:[]});
 });
 
+// Definicion de las rutas de los comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
